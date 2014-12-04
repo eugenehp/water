b5b3b64320ecdf5b87a4aefa454b5f4f984e53c3
Eugene HP <eugene.hp2012@gmail.com>
2014-12-04 20:31:06 +0100

    a little bit optimized version, remove unnecessary code

--- main.js
+++ main.js
@@ -145,29 +145,29 @@ window.onload = function() {
         water.addDrop(pointOnPlane.x, pointOnPlane.z, 0.03, 0.01);
         if (paused) {
           water.updateNormals();
-          renderer.updateCaustics(water);
+          // renderer.updateCaustics(water);
         }
         break;
       }
-      case MODE_MOVE_SPHERE: {
-        var tracer = new GL.Raytracer();
-        var ray = tracer.getRayForPixel(x * ratio, y * ratio);
-        var t = -planeNormal.dot(tracer.eye.subtract(prevHit)) / planeNormal.dot(ray);
-        var nextHit = tracer.eye.add(ray.multiply(t));
-        center = center.add(nextHit.subtract(prevHit));
-        center.x = Math.max(radius - 1, Math.min(1 - radius, center.x));
-        center.y = Math.max(radius - 1, Math.min(10, center.y));
-        center.z = Math.max(radius - 1, Math.min(1 - radius, center.z));
-        prevHit = nextHit;
-        if (paused) renderer.updateCaustics(water);
-        break;
-      }
-      case MODE_ORBIT_CAMERA: {
-        angleY -= x - oldX;
-        angleX -= y - oldY;
-        angleX = Math.max(-89.999, Math.min(89.999, angleX));
-        break;
-      }
+      // case MODE_MOVE_SPHERE: {
+      //   var tracer = new GL.Raytracer();
+      //   var ray = tracer.getRayForPixel(x * ratio, y * ratio);
+      //   var t = -planeNormal.dot(tracer.eye.subtract(prevHit)) / planeNormal.dot(ray);
+      //   var nextHit = tracer.eye.add(ray.multiply(t));
+      //   center = center.add(nextHit.subtract(prevHit));
+      //   center.x = Math.max(radius - 1, Math.min(1 - radius, center.x));
+      //   center.y = Math.max(radius - 1, Math.min(10, center.y));
+      //   center.z = Math.max(radius - 1, Math.min(1 - radius, center.z));
+      //   prevHit = nextHit;
+      //   // if (paused) renderer.updateCaustics(water);
+      //   break;
+      // }
+      // case MODE_ORBIT_CAMERA: {
+      //   angleY -= x - oldX;
+      //   angleX -= y - oldY;
+      //   angleX = Math.max(-89.999, Math.min(89.999, angleX));
+      //   break;
+      // }
     }
     oldX = x;
     oldY = y;
